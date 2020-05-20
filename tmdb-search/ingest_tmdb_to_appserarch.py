69d35e90c6b03000d1b6210322fa1a9a704b12da
xuyundong <xuyundong@douban.com>
2020-05-20 08:19:22 +0800

    Feat: upgrade tmdb-search to py3

--- tmdb-search/ingest_tmdb_to_appserarch.py
+++ tmdb-search/ingest_tmdb_to_appserarch.py
@@ -1,34 +1,39 @@
-import requests
 import json
+
+import requests
 
 
 def extract():
     f = open('./tmdb.json')
     if f:
-         return json.loads(f.read());        
+        return json.loads(f.read())
     return {}
 
 
-def index_all(movieDict={}):
-
-    for id, movie in movieDict.iteritems(): 
+def index_all(movieDict=None):
+    movieDict = movieDict or {}
+    for id, movie in movieDict.items():
         index_doc(movie)
         print(id)
 
 
 def index_doc(doc):
-    content=[]
+    content = []
     content.append(doc)
-    resp = requests.post("http://localhost:3002/api/as/v1/engines/tmdb/documents", 
-        headers={"content-type":"application/json","Authorization": "Bearer private-vmnsw8mvzebbcpfeje5rzhxg"}, 
+    resp = requests.post(
+        "http://localhost:3002/api/as/v1/engines/tmdb/documents",
+        headers={
+            "content-type": "application/json",
+            "Authorization": "Bearer private-vmnsw8mvzebbcpfeje5rzhxg"
+        },
         data=json.dumps(content))
-    print resp
-
+    print(resp)
 
 
 def main():
     movieDict = extract()
     index_all(movieDict=movieDict)
-  
-if __name__== "__main__":
-  main()    
+
+
+if __name__ == "__main__":
+    main()
